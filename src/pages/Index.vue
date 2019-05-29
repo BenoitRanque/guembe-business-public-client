a050fb87870a4260369d2fb94124ac3f1e0ac320
BenoitRanque <ranque.benoit@gmail.com>
2019-05-29 04:19:39 -0400

    upgraded quasar packages

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@
     <q-img contain style="max-height: 60vh; max-width: 60vw;" src="assets/isologo.svg" ></q-img>
     <q-btn flat @click="$router.push('/listings')" color="secondary" icon-right="mdi-forward">Ir A Tienda</q-btn>
     <q-btn flat type="a" :href="authUrl  + '/google'" color="secondary" icon-right="mdi-forward">Test</q-btn>
+    <!-- <q-btn flat @click="test" color="secondary" icon-right="mdi-forward">Test</q-btn> -->
     <pre>{{$store.getters.session}}</pre>
     <pre>{{result}}</pre>
   </q-page>
@@ -22,6 +23,17 @@ export default {
   computed: {
     authUrl () {
       return `${this.$api.defaults.baseURL}/auth/authenticate`
+    }
+  },
+  methods: {
+    async test () {
+      try {
+        const { data } = await this.$api('/auth/authenticate/google')
+
+        this.result = data
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
